749f6a767773aeb5dafffdb6dfbd5f054a5716ee
Connor Gill <connor@connorgill.com>
2020-06-03 18:35:11 -0700

    Update testCircularBarPlot.py
    Added more plot points, removed grid, looks more like rehoboam now.

--- testCircularBarPlot.py
+++ testCircularBarPlot.py
@@ -1,7 +1,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 80
+#~~~~~~~~~~~Test~~~~~~~#
+import pandas as pd
+import seaborn as sns
+# Import Data
+df = pd.read_csv("https://github.com/selva86/datasets/raw/master/mpg_ggplot2.csv")
+#~~~~~~~~~~~Test~~~~~~~#
+
+N = 1000
 bottom = 8
 max_height = 4
 
@@ -12,9 +19,14 @@ width = (2*np.pi) / N
 ax = plt.subplot(111, polar=True)
 bars = ax.bar(theta, radii, width=width, bottom=bottom)
 
+
+
 # Use custom colors and opacity
 for r, bar in zip(radii, bars):
-    bar.set_facecolor(plt.cm.jet(r / 10.))
-    bar.set_alpha(0.8)
+    bar.set_facecolor('000000')
+    #bar.set_facecolor(plt.cm.jet(r / 10.))
+    #bar.set_alpha(0.9)
 
+plt.title('Rehoboam', fontsize=22)
+plt.axis('off')
 plt.show()
