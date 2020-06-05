5707b2cf450ed44cd9e2236953306700cc34a81b
Connor Gill <connor@connorgill.com>
2020-06-04 22:58:34 -0700

    Add files via upload
    Added mySQL database support. Now pulls data directly from AWS RDS MySQL database

--- rehoV1.py
+++ rehoV1.py
@@ -1,22 +1,37 @@
+########################
+#Import dependencies
+########################
 import numpy as np
 import matplotlib.pyplot as plt
+import mpld3
+import MySQLdb
 
 
-#~~~~~~~~~~~Test~~~~~~~#
-#import pandas as pd
-#import seaborn as sns
-#import csv
-# Import Data
-#df = pd.read_csv("https://github.com/selva86/datasets/raw/master/mpg_ggplot2.csv")
-#~~~~~~~~~~~Test~~~~~~~#
+#~~~~~~~~~~~Importing mySQL Data~~~~~~~#
+db = MySQLdb.connect("database-1.cluster-ro-cagxsdx2k0ey.us-east-2.rds.amazonaws.com", "admin", "rehoboam")
+cursor = db.cursor()
+sql1 = "SELECT theta FROM rehoboamSchema.rehoboamFull"
+cursor.execute(sql1)
 
-N = 3000    #Numbers of bars
+thetaArray = list(cursor.fetchall())
+thetaSQL = np.array(thetaArray).ravel() #converts to contiguous list
+
+sql2 = "SELECT radii FROM rehoboamSchema.rehoboamFull"
+cursor.execute(sql2)
+
+radiiArray = list(cursor.fetchall())
+radiiSQL = np.array(radiiArray).ravel() #converts to contiguous list
+#~~~~~~~~~~~mySQL~~~~~~~#
+
+N = 2000    #Numbers of bars
 bottom = 8  #Where bars bottoms begin
 max_height = .5 
-
-theta = np.linspace(0.0, 2 * np.pi, N, endpoint=False) #array of indexes for each bar
-radii = max_height*np.random.rand(N)    #array of heights/radii for each bar
 width = (8*np.pi) / N   #width of each bar
+
+#~~~~~~Original Calculation of theta & radii~~~~~#
+#theta = np.linspace(0.0, 2 * np.pi, N, endpoint=False) #array of indexes for each bar
+#radii = max_height*np.random.rand(N)    #array of heights/radii for each bar
+#~~~~~~~~~~~~~~~~~~~~#
 
 #Printing radii values out to CSV
 #file = open('radiiout.csv', 'a') 
@@ -32,12 +47,12 @@ width = (8*np.pi) / N   #width of each bar
 
 #Sets bars and plot
 ax = plt.subplot(111, polar=True)
-bars = ax.bar(theta, radii, width=width, bottom=bottom) 
+bars = ax.bar(thetaSQL, radiiSQL, width=width, bottom=bottom) 
 
 
 
 # Use custom colors and opacity
-for r, bar in zip(radii, bars):
+for r, bar in zip(radiiSQL, bars):
     bar.set_facecolor('000000')
     #bar.set_facecolor(plt.cm.jet(r / 10.))
     #bar.set_alpha(0.9)
