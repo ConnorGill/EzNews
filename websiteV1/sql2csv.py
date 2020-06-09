647a48b0a65ed7daf3ef757c5ff4112fd254bc0c
Connor Gill <connordgill@gmail.com>
2020-06-09 13:09:21 -0700

    Python to JS
    Had to recreate the chart in JS for website support.

---
+++ websiteV1/sql2csv.py
@@ -0,0 +1,43 @@
+import MySQLdb
+import sys
+import csv
+
+
+def sql2csv():
+    db = MySQLdb.connect("database-1.cluster-ro-cagxsdx2k0ey.us-east-2.rds.amazonaws.com", "admin", "rehoboam")
+    cursor = db.cursor()
+
+    sql1 = "SELECT indexKey, radii FROM rehoboamSchema.vw_rehoboam"
+    cursor.execute(sql1)
+    result=cursor.fetchall()
+
+    c = csv.writer(open('rehoTestData.csv', 'w', newline=''))
+    c.writerow(["indexKey", "Value"])
+    for x in result:
+        c.writerow(x) 
+
+    db.close()
+
+
+def radiiNoiseGen():
+    db = MySQLdb.connect("database-1.cluster-ro-cagxsdx2k0ey.us-east-2.rds.amazonaws.com", "admin", "rehoboam")
+    cursor = db.cursor()
+    sql4 = ("UPDATE rehoboamSchema.rehoboamFull SET RADII = FLOOR(RAND()*(5000-100) + 100) #END")
+    cursor.execute(sql4)
+    db.commit()
+    #sql2 = ("UPDATE rehoboamSchema.rehoboamFull SET RADII = RADII * 2 WHERE RADII < .00025")
+    #cursor.execute(sql2)
+    #db.commit()
+    db.close()
+
+
+radiiNoiseGen()
+sql2csv()
+"""
+count = 0
+while (count < 100):
+    radiiNoiseGen()
+    sql2csv()
+    count = count + 1
+    print(count)
+"""
